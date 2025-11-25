2ba15642c3c73a1685a20eb55413a71fb74c2b24
Kolyan4ik316 <Kolyan4ikn1@gmail.com>
2025-11-25 04:54:17 +0200

    Lil bugfixes

--- Source/ProjectOne/M_InputDeviceSubsystem/M_InputDeviceSubsystem.cpp
+++ Source/ProjectOne/M_InputDeviceSubsystem/M_InputDeviceSubsystem.cpp
@@ -8,11 +8,32 @@ UM_InputDeviceSubsystem::UM_InputDeviceSubsystem()
 
 }
 
-void UM_InputDeviceSubsystem::ToggleSlateNavigation(bool bInTabNavigation, bool bInKeyNavigation, bool bInAnalogNavigation)
+void UM_InputDeviceSubsystem::ToggleSlateNavigation(bool bInTabNavigation, bool bInKeyNavigation, bool bInAnalogNavigation, bool bInActionNavigation)
 {
+    if (FSlateApplication::IsInitialized())
+    {
 		TSharedRef<FNavigationConfig> navigation_config = FSlateApplication::Get().GetNavigationConfig();
 		navigation_config->bTabNavigation = bInTabNavigation;
 		navigation_config->bKeyNavigation = bInKeyNavigation;
 		navigation_config->bAnalogNavigation = bInAnalogNavigation;
+
+		if (bInActionNavigation)
+        {
+            // Якщо увімкнено - повертаємо правила назад, якщо їх немає
+            if (!navigation_config->KeyActionRules.Contains(EKeys::Enter))
+            {
+                navigation_config->KeyActionRules.Emplace(EKeys::Enter, EUINavigationAction::Accept);
+                navigation_config->KeyActionRules.Emplace(EKeys::SpaceBar, EUINavigationAction::Accept);
+                navigation_config->KeyActionRules.Emplace(EKeys::Virtual_Accept, EUINavigationAction::Accept);
+            }
+        }
+        else
+        {
+            navigation_config->KeyActionRules.Remove(EKeys::Enter);
+            navigation_config->KeyActionRules.Remove(EKeys::SpaceBar);
+            navigation_config->KeyActionRules.Remove(EKeys::Virtual_Accept); 
+        }
+
 		FSlateApplication::Get().SetNavigationConfig(navigation_config);
+	}
 }
--- Source/ProjectOne/M_InputDeviceSubsystem/M_InputDeviceSubsystem.h
+++ Source/ProjectOne/M_InputDeviceSubsystem/M_InputDeviceSubsystem.h
@@ -27,6 +27,6 @@ protected:
 	//TSharedRef<FNavigationConfig> DisabledNavigationConfig = MakeShared<FNavigationConfig>();
 public:
 	UFUNCTION(BlueprintCallable, Category = "Input|Navigation")
-	void ToggleSlateNavigation(bool bInTabNavigation, bool bInKeyNavigation, bool bInAnalogNavigation);
+	void ToggleSlateNavigation(bool bInTabNavigation = true, bool bInKeyNavigation = true, bool bInAnalogNavigation = true, bool bInActionNavigation = true);
 	
 };
